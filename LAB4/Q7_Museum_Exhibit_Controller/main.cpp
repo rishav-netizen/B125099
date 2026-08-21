97a69e92a1ba4a6df0c4b70dc369309ce60c4c73
rishav-netizen <rishavkumar2k07@gmail.com>
2026-08-21 17:40:54 +0530

    solved question 7 with output

--- LAB4/Q7_Museum_Exhibit_Controller/main.cpp
+++ LAB4/Q7_Museum_Exhibit_Controller/main.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class MuseumManager;
 
 class Exhibit {
-private:
-    string exhibitName;
-    int exhibitID;
-    int visitorCount;
-    bool displayStatus;
+    private:
+        string exhibitName;
+        int exhibitID;
+        int visitorCount;
+        bool displayStatus;
 
-public:
-    Exhibit(string name, int id) {
-        exhibitName = name;
-        exhibitID = id;
-        visitorCount = 0;
-        displayStatus = false;
-    }
+    public:
+        void Input() {
+            cout << "Enter exhibit name: ";
+            getline(cin, exhibitName);
 
-    friend class MuseumManager;
+            cout << "Enter exhibit ID: ";
+            cin >> exhibitID;
+
+            cout << "Enter visitor count: ";
+            cin >> visitorCount;
+
+            cout << "Enter display status (1 for Open, 0 for Closed): ";
+            cin >> displayStatus;
+        }
+        Exhibit()
+        {
+
+        }
+
+        Exhibit(string name, int id) {
+            exhibitName = name;
+            exhibitID = id;
+            visitorCount = 0;
+            displayStatus = false;
+        }
+
+        friend class MuseumManager;
 };
 
 class MuseumManager {
 public:
     void displayInfo(Exhibit &e) {
-        cout << "Exhibit: " << e.exhibitName << endl;
+        cout << "\nData Regarding Exibit\nExhibit: " << e.exhibitName << endl;
         cout << "Exhibit ID: " << e.exhibitID << endl;
         cout << "Visitors: " << e.visitorCount << endl;
         cout << "Status: " << (e.displayStatus ? "Open" : "Closed") << endl;
@@ -48,13 +67,16 @@ public:
     }
 
     void checkStatus(Exhibit &e) {
+
         cout << (e.displayStatus ? "Exhibit is Open" : "Exhibit is Closed")
              << endl;
     }
 };
 
 int main() {
-    Exhibit e("Ancient Art", 101);
+    Exhibit e;
+    e.Input();
+
     MuseumManager manager;
 
     manager.openExhibit(e);
