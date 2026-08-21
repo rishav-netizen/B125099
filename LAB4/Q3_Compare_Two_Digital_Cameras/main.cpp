5f262e995d20ffe800d4ccedd90bc4995177faec
rishav-netizen <rishavkumar2k07@gmail.com>
2026-08-21 17:01:58 +0530

    added solution for q3 with output

--- LAB4/Q3_Compare_Two_Digital_Cameras/main.cpp
+++ LAB4/Q3_Compare_Two_Digital_Cameras/main.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 class Camera {
-private:
-    string brand;
-    string model;
-    int megapixels;
-    int storageCapacity;
+    private:
+        string brand;
+        string model;
+        int megapixels;
+        int storageCapacity;
 
-public:
-    Camera(string b, string m, int mp, int storage) {
-        brand = b;
-        model = m;
-        megapixels = mp;
-        storageCapacity = storage;
-    }
+    public:
+        Camera()
+        {
 
-    friend void compareCamera(Camera c1, Camera c2);
+        }
+
+        Camera(string b, string m, int mp, int storage) {
+            brand = b;
+            model = m;
+            megapixels = mp;
+            storageCapacity = storage;
+        }
+
+        void Input()
+        {
+            cout << "Brand: ";
+            cin.ignore();
+            getline(cin, brand);
+            cout << "Model: ";
+            cin >> model;
+            cout << "Megapixels: ";
+            cin >> megapixels;
+            cout << "Storage(GB): ";
+            cin >> storageCapacity;
+        }
+
+
+        friend void compareCamera(Camera c1, Camera c2);
 };
 
 void compareCamera(Camera c1, Camera c2) {
     Camera better = c1;
 
-    if (c2.megapixels > c1.megapixels ||
-        (c2.megapixels == c1.megapixels &&
-         c2.storageCapacity > c1.storageCapacity)) {
+    if (c2.megapixels > c1.megapixels || (c2.megapixels == c1.megapixels && c2.storageCapacity > c1.storageCapacity)) 
+    {
         better = c2;
     }
 
-    cout << "Better Camera:" << endl;
+    cout << "\n\nDetails of the better camera are as follows:\n";
     cout << "Brand: " << better.brand << endl;
     cout << "Model: " << better.model << endl;
     cout << "Megapixels: " << better.megapixels << endl;
-    cout << "Storage: " << better.storageCapacity << " GB" << endl;
+    cout << "Storage: " << better.storageCapacity << " GB\n";
 }
 
 int main() {
-    Camera c1("Canon", "R50", 24, 128);
-    Camera c2("Sony", "A6400", 26, 64);
+    Camera *cameras = new Camera[2];
 
-    compareCamera(c1, c2);
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "\nEnter camera " << i + 1 << " details: \n";
+        cameras[i].Input();
+    }
+    
+    compareCamera(cameras[0], cameras[1]);
 
     return 0;
 }
