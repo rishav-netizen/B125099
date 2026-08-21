e132d5a741710ebf9e81cfd5a8a314043259d890
rishav-netizen <rishavkumar2k07@gmail.com>
2026-08-21 17:47:03 +0530

    solved question 9 with output

--- LAB4/Q9_Digital_Wallet_Controller/main.cpp
+++ LAB4/Q9_Digital_Wallet_Controller/main.cpp
@@ -1,64 +1,83 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class WalletManager;
 
 class DigitalWallet {
-private:
-    string userName;
-    double walletBalance;
-    bool walletStatus;
+    private:
+        string userName;
+        double walletBalance;
+        bool walletStatus;
 
-public:
-    DigitalWallet(string name, double balance, bool status) {
-        userName = name;
-        walletBalance = balance;
-        walletStatus = status;
-    }
+    public:
+        DigitalWallet(string name, double balance, bool status) {
+            userName = name;
+            walletBalance = balance;
+            walletStatus = status;
+        }
 
-    friend class WalletManager;
+        void Input() {
+            cout << "Enter user name: ";
+            getline(cin, userName);
+
+            cout << "Enter wallet balance: ";
+            cin >> walletBalance;
+
+            cout << "Enter wallet status (1 for Active, 0 for Disabled): ";
+            cin >> walletStatus;
+        }
+
+        friend class WalletManager;
 };
 
 class WalletManager {
-public:
-    void displayDetails(DigitalWallet &w) {
-        cout << "User: " << w.userName << endl;
-        cout << "Balance: Rs. " << w.walletBalance << endl;
-        cout << "Status: " << (w.walletStatus ? "Active" : "Disabled")
-             << endl;
-    }
+    public:
+        void displayDetails(DigitalWallet &w) {
+            cout << "\n\n--USER DETAILS--\nUser: " << w.userName << endl;
+            cout << "Balance: Rs. " << w.walletBalance << endl;
+            cout << "Status: " << (w.walletStatus ? "Active" : "Disabled")
+                 << endl;
+        }
 
-    void addMoney(DigitalWallet &w, double amount) {
-        if (w.walletStatus)
-            w.walletBalance += amount;
-    }
+        void addMoney(DigitalWallet &w, double amount) {
+            if (w.walletStatus)
+                w.walletBalance += amount;
+        }
 
-    void deductMoney(DigitalWallet &w, double amount) {
-        if (w.walletStatus && amount <= w.walletBalance)
-            w.walletBalance -= amount;
-        else
-            cout << "Insufficient balance or wallet disabled." << endl;
-    }
+        void deductMoney(DigitalWallet &w, double amount) {
+            if (w.walletStatus && amount <= w.walletBalance)
+                w.walletBalance -= amount;
+            else
+                cout << "Insufficient balance or wallet disabled." << endl;
+        }
 
-    void disableWallet(DigitalWallet &w) {
-        w.walletStatus = false;
-    }
+        void disableWallet(DigitalWallet &w) {
+            w.walletStatus = false;
+        }
 
-    void displayStatus(DigitalWallet &w) {
-        cout << "Wallet Status: "
-             << (w.walletStatus ? "Active" : "Disabled") << endl;
-    }
+        void displayStatus(DigitalWallet &w) {
+            cout << "Wallet Status: "
+                 << (w.walletStatus ? "Active" : "Disabled") << endl;
+        }
 };
 
 int main() {
-    DigitalWallet wallet("Rishav", 1000, true);
+    DigitalWallet wallet("", 0, false);
     WalletManager manager;
+
+    wallet.Input();
 
     manager.displayDetails(wallet);
 
-    manager.addMoney(wallet, 500);
-    manager.deductMoney(wallet, 300);
+    int added, deducted;
+    cout << "Enter amount to add: ";
+    cin >> added;
+    cout << "Enter amount to use: ";
+    cin >> deducted;
+    manager.addMoney(wallet, added);
+    manager.deductMoney(wallet, deducted);
 
     cout << "\nAfter Transactions:\n";
     manager.displayDetails(wallet);
