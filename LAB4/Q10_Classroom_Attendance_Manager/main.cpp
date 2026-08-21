cad777bfbbb7406d7727c97ea2546ba3d3ae51a4
rishav-netizen <rishavkumar2k07@gmail.com>
2026-08-21 17:51:11 +0530

    solved q10 with output

--- LAB4/Q10_Classroom_Attendance_Manager/main.cpp
+++ LAB4/Q10_Classroom_Attendance_Manager/main.cpp
@@ -1,65 +1,88 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class AttendanceManager;
 
 class Classroom {
-private:
-    string className;
-    int totalStudents;
-    int presentStudents;
-    bool attendanceStatus;
+    private:
+        string className;
+        int totalStudents;
+        int presentStudents;
+        bool attendanceStatus;
 
-public:
-    Classroom(string name, int total) {
-        className = name;
-        totalStudents = total;
-        presentStudents = 0;
-        attendanceStatus = false;
-    }
+    public:
+        Classroom() {}
+        Classroom(string name, int total) {
+            className = name;
+            totalStudents = total;
+            presentStudents = 0;
+            attendanceStatus = false;
+        }
 
-    friend class AttendanceManager;
+        void Input() {
+            cout << "Enter class name: ";
+            getline(cin, className);
+
+            cout << "Enter total students: ";
+            cin >> totalStudents;
+
+            cout << "Enter present students: ";
+            cin >> presentStudents;
+            
+            while (presentStudents > totalStudents)
+            {
+                cout << "Invalid Input\nEnter present students: ";
+                cin >> presentStudents;
+            }
+            
+
+            cout << "Enter attendance status (1 for Completed, 0 for Not Completed): ";
+            cin >> attendanceStatus;
+        }
+
+        friend class AttendanceManager;
 };
 
 class AttendanceManager {
-public:
-    void displayInfo(Classroom &c) {
-        cout << "Class: " << c.className << endl;
-        cout << "Total Students: " << c.totalStudents << endl;
-        cout << "Present Students: " << c.presentStudents << endl;
-        cout << "Attendance: "
-             << (c.attendanceStatus ? "Completed" : "Not Completed")
-             << endl;
-    }
+    public:
+        void displayInfo(Classroom &c) {
+            cout << "\n--CLASS DETAILS--\n";
+            cout << "Class: " << c.className << endl;
+            cout << "Total Students: " << c.totalStudents << endl;
+            cout << "Present Students: " << c.presentStudents << endl;
+            cout << "Attendance: "
+                 << (c.attendanceStatus ? "Completed" : "Not Completed")
+                 << endl;
+        }
 
-    void updatePresent(Classroom &c, int present) {
-        if (present >= 0 && present <= c.totalStudents)
-            c.presentStudents = present;
-    }
+        void updatePresent(Classroom &c, int present) {
+            if (present >= 0 && present <= c.totalStudents)
+                c.presentStudents = present;
+        }
 
-    void completeAttendance(Classroom &c) {
-        c.attendanceStatus = true;
-    }
+        void completeAttendance(Classroom &c) {
+            c.attendanceStatus = true;
+        }
 
-    void displayStatus(Classroom &c) {
-        cout << "Attendance Status: "
-             << (c.attendanceStatus ? "Completed" : "Not Completed")
-             << endl;
-    }
+        void displayStatus(Classroom &c) {
+            cout << "Attendance Status: "
+                 << (c.attendanceStatus ? "Completed" : "Not Completed")
+                 << endl;
+        }
 
-    void calculateAbsent(Classroom &c) {
-        int absent = c.totalStudents - c.presentStudents;
-        cout << "Absent Students: " << absent << endl;
-    }
+        void calculateAbsent(Classroom &c) {
+            int absent = c.totalStudents - c.presentStudents;
+            cout << "Absent Students: " << absent << endl;
+        }
 };
 
 int main() {
-    Classroom c("CSE B1", 60);
+    Classroom c;
     AttendanceManager manager;
 
-    manager.updatePresent(c, 52);
-    manager.completeAttendance(c);
+    c.Input();
 
     manager.displayInfo(c);
     manager.calculateAbsent(c);
